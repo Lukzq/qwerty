b75a21eb7cac637545a9b85ac6028505b72ceb1c
Lukzq <96998553+Lukzq@users.noreply.github.com>
2022-01-02 19:25:21 -0300

    me muero

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-let mySprite = sprites.create(assets.image`miImagen`, SpriteKind.Player)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    game.reset()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy += -100
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`sus`, SpriteKind.Player)
+tiles.setTilemap(tilemap`nivel1`)
+tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass2)
+scene.cameraFollowSprite(mySprite)
+mySprite.setVelocity(100, 37)
+mySprite.ay = 200
